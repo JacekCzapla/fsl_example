e6355b8109e60b8071a2e6ec257161dea78cbb92
Jacek Czapla <jczapla@asit.pl>
2019-11-20 12:05:17 +0100

    changed example with 2 fuzzy variables

--- Fuzzy_FLS_test/Program.cs
+++ Fuzzy_FLS_test/Program.cs
@@ -17,17 +17,28 @@ namespace Fuzzy_FLS_test
             var warm = water.MembershipFunctions.AddTriangle("Warm", 30, 50, 70);
             var hot = water.MembershipFunctions.AddTrapezoid("Hot", 50, 80, 100, 100);
 
+            var sugar = new LinguisticVariable("Sugar");
+            var big = water.MembershipFunctions.AddTrapezoid("big", 0, 0, 20, 40);
+            var small = water.MembershipFunctions.AddTrapezoid("small", 50, 80, 100, 100);
+
+
             var power = new LinguisticVariable("Power");
             var low = power.MembershipFunctions.AddTriangle("Low", 0, 25, 50);
-            var high = power.MembershipFunctions.AddTriangle("High", 25, 50, 75);
+            var medium = power.MembershipFunctions.AddTriangle("Medium", 25, 50, 75);
+            var high = power.MembershipFunctions.AddTriangle("High", 50, 75, 100);
 
             IFuzzyEngine fuzzyEngine = new FuzzyEngineFactory().Default();
 
-            var rule1 = Rule.If(water.Is(cold).Or(water.Is(warm))).Then(power.Is(high));
-            var rule2 = Rule.If(water.Is(hot)).Then(power.Is(low));
-            fuzzyEngine.Rules.Add(rule1, rule2);
 
-            var result = fuzzyEngine.Defuzzify(new { water = 90 });
+            var rule1 = Rule.If(water.Is(cold).And(sugar.Is(big))).Then(power.Is(high));
+            var rule2 = Rule.If(water.Is(cold).And(sugar.Is(small))).Then(power.Is(medium));
+            var rule3 = Rule.If(water.Is(hot).And(sugar.Is(big))).Then(power.Is(medium));
+            var rule4 = Rule.If(water.Is(hot).And(sugar.Is(small))).Then(power.Is(low));
+
+            
+            fuzzyEngine.Rules.Add(rule1, rule2, rule3, rule4);
+
+            var result = fuzzyEngine.Defuzzify(new { water = 90, sugar=30 });
         }
     }
 }
